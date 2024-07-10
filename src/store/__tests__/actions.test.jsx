9c2c8bd961b9a4043557d6032a65733ec13780b3
Iv3tt3 <reach.iv3tt3@gmail.com>
2024-07-10 14:53:23 +0100

    async test

--- src/store/__tests__/actions.test.jsx
+++ src/store/__tests__/actions.test.jsx
@@ -1,6 +1,6 @@
-import { advertsLoadedFulfilled, authLoginPending } from '../actions';
+import { advertsLoadedFulfilled, authLogin, authLoginFulfilled, authLoginPending, authLoginRejected } from '../actions';
 import { ADVERTS_LOADED_FULFILLED, AUTH_LOGIN_PENDING } from '../types';
-import { describe, expect, it } from "vitest";
+import { afterEach, describe, expect, it, vi } from "vitest";
 
 
 describe('authLoginPending', () => {
@@ -22,5 +22,46 @@ describe('advertsLoadedFulfilled', () => {
     };
     const action = advertsLoadedFulfilled(adverts);
     expect(action).toEqual(expectedAction);
+  });
+});
+
+describe('authLogin', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  const credentials = 'credentials';
+  const checked = 'checked'
+  const action = authLogin(credentials, checked);
+
+  const redirectUrl = 'redirectUrl';
+  const dispatch = vi.fn();
+  const services = { auth: {} };
+  const router = {
+    state: { location: { state: { pathname: redirectUrl } } },
+    navigate: vi.fn(),
+  };
+
+  it('when login resolves should follow the login flow', async () => {
+    services.auth.login = vi.fn().mockResolvedValue();
+
+    await action(dispatch, undefined, { services, router });
+    expect(dispatch).toHaveBeenCalledTimes(2);
+    expect(dispatch).toHaveBeenNthCalledWith(1, authLoginPending());
+    expect(services.auth.login).toHaveBeenCalledWith(credentials, checked);
+    expect(dispatch).toHaveBeenNthCalledWith(2, authLoginFulfilled());
+    expect(router.navigate).toHaveBeenCalledWith(redirectUrl);
+  });
+
+  it('when login rejects should follow the error flow', async () => {
+    const error = new Error('Unauthorized');
+    services.auth.login = vi.fn().mockRejectedValue(error);
+
+    await action(dispatch, undefined, { services, router });
+    expect(dispatch).toHaveBeenCalledTimes(2);
+    expect(dispatch).toHaveBeenNthCalledWith(1, authLoginPending());
+    expect(services.auth.login).toHaveBeenCalledWith(credentials, checked);
+    expect(dispatch).toHaveBeenNthCalledWith(2, authLoginRejected(error));
+    expect(router.navigate).not.toHaveBeenCalled();
   });
 });
